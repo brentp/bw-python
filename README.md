7a89d3c73e7f5677beea04a55b811f113d116fe8
Brent Pedersen (brentp) <bpederse@gmail.com>
2015-10-20 08:50:08 -0600

    change default to return nan's for empty

--- README.md
+++ README.md
@@ -12,14 +12,13 @@ Interval(chrom='1', start=1, end=2, value=0.20000000298023224)
 Interval(chrom='1', start=2, end=3, value=0.30000001192092896)
 
 # get an array.array() for large numbers of values.
-# default is to return only sites with values.
+# default is to return nan's for missing values
 >>> b.values("1", 0, 9)
-array('f', [0.10000000149011612, 0.20000000298023224, 0.30000001192092896])
-# return nan for empties
->>> b.values("1", 0, 9, True)
 array('f', [0.10000000149011612, 0.20000000298023224, 0.30000001192092896, nan, nan, nan, nan, nan, nan])
 
-
+# we can also get missing, but won't know the base it's associated with. 
+>>> b.values("1", 0, 9, False)
+array('f', [0.10000000149011612, 0.20000000298023224, 0.30000001192092896])
 
 >>> b.close()
 ```
--- bw/bw.py
+++ bw/bw.py
@@ -16,10 +16,11 @@ class BigWig(object):
     Interval(chrom='1', start=1, end=2, value=0.20000000298023224)
     Interval(chrom='1', start=2, end=3, value=0.30000001192092896)
 
+    # default is to include all values
+    >>> b.values("1", 0, 9)
+    array('f', [0.10000000149011612, 0.20000000298023224, 0.30000001192092896, nan, nan, nan, nan, nan, nan])
     >>> b.values("1", 0, 9, False)
     array('f', [0.10000000149011612, 0.20000000298023224, 0.30000001192092896])
-    >>> b.values("1", 0, 9, True)
-    array('f', [0.10000000149011612, 0.20000000298023224, 0.30000001192092896, nan, nan, nan, nan, nan, nan])
 
     >>> b.close()
     """
@@ -40,7 +41,7 @@ class BigWig(object):
             yield Interval(chrom, intervals.start[i], intervals.start[i] + 1, intervals.value[i])
         lib.bwDestroyOverlappingIntervals(intervals)
 
-    def values(self, chrom, start, end, includeNA=False):
+    def values(self, chrom, start, end, includeNA=True):
         intervals = lib.bwGetValues(self.bw, chrom, start, end, int(includeNA))
         a = array.array('f')
         if intervals.l != 0:
