55103fa58b6b37508b09ff85289b16d30dfe8392
Brent Pedersen (brentp) <bpederse@gmail.com>
2015-10-19 20:36:12 -0600

    make an array.array directly from the buffer

--- bw/bw.py
+++ bw/bw.py
@@ -1,5 +1,6 @@
 from ._bigwig import lib, ffi
 from collections import namedtuple
+import array
 
 Interval = namedtuple('Interval', ['chrom', 'start', 'end', 'value'])
 
@@ -14,6 +15,11 @@ class BigWig(object):
     Interval(chrom='1', start=0, end=1, value=0.10000000149011612)
     Interval(chrom='1', start=1, end=2, value=0.20000000298023224)
     Interval(chrom='1', start=2, end=3, value=0.30000001192092896)
+
+    >>> b.values("1", 0, 9, False)
+    array('f', [0.10000000149011612, 0.20000000298023224, 0.30000001192092896])
+    >>> b.values("1", 0, 9, True)
+    array('f', [0.10000000149011612, 0.20000000298023224, 0.30000001192092896, nan, nan, nan, nan, nan, nan])
 
     >>> b.close()
     """
@@ -33,6 +39,14 @@ class BigWig(object):
         for i in range(intervals.l):
             yield Interval(chrom, intervals.start[i], intervals.start[i] + 1, intervals.value[i])
         lib.bwDestroyOverlappingIntervals(intervals)
+
+    def values(self, chrom, start, end, includeNA=False):
+        intervals = lib.bwGetValues(self.bw, chrom, start, end, int(includeNA))
+        a = array.array('f')
+        if intervals.l != 0:
+            a.fromstring(ffi.buffer(intervals.value[0:intervals.l]))
+        lib.bwDestroyOverlappingIntervals(intervals)
+        return a
 
 if __name__ == "__main__":
     import doctest
