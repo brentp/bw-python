6618bc060001b4fa681162b07735e85a20b423c6
Brent Pedersen (brentp) <bpederse@gmail.com>
2015-10-27 13:52:05 -0600

    implement stats

--- bw/build_bw.py
+++ bw/build_bw.py
@@ -18,12 +18,13 @@ ffi = FFI()
 
 #include "curl/curl.h"
 
-sources=glob.glob("{path}/libBigWig/*.c".format(path=HERE))
-print sources
+sources = glob.glob("{path}/libBigWig/*.c".format(path=HERE))
+#print sources
 #include "{path}/bw/curl_constants.h"
 #include "{path}/libBigWig/bwValues.h"
 ffi.set_source("bw._bigwig", """
 #include "{path}/libBigWig/bigWig.h"
+#include <stdlib.h>
 """.format(path=HERE),
                libraries=["c", "curl"],
                sources=sources,
@@ -54,19 +55,25 @@ void bwClose(bigWigFile_t *fp);
 bwOverlappingIntervals_t *bwGetValues(bigWigFile_t *fp, char *chrom, uint32_t start, uint32_t end, int includeNA);
 void bwDestroyOverlappingIntervals(bwOverlappingIntervals_t *o);
 
+enum bwStatsType {
+    doesNotExist = -1,
+    mean = 0,
+    average = 0,
+    std = 1,
+    stdev = 1,
+    dev = 1,
+    max = 2,
+    min = 3,
+    cov = 4,
+    coverage = 4,
+};
+
+double *bwStats(bigWigFile_t *fp, char *chrom, uint32_t start, uint32_t end, uint32_t nBins, enum bwStatsType type);
+
+void free(void *);
+
 \n""")
 
-
-#typedef struct { ...; } bigWigFile_t;
-
-#with open("%s/libBigWig/io.h" % HERE) as fh:
-#   ffi.cdef("\n".join(x for x in fh if not x.startswith("#")))
-
-#with open("%s/libBigWig/bwValues.h" % HERE) as fh:
-#   ffi.cdef("\n".join(x for x in fh if not x.startswith("#")), override=True)
-
-#with open("%s/libBigWig/bigWig.h" % HERE) as fh:
-#    ffi.cdef("\n".join(x for x in fh if not x.startswith("#")))
 
 if __name__ == "__main__":
     ffi.compile()
--- bw/bw.py
+++ bw/bw.py
@@ -4,7 +4,6 @@ import array
 
 Interval = namedtuple('Interval', ['chrom', 'start', 'end', 'value'])
 
-
 class BigWig(object):
     """
     >>> b = BigWig("libBigWig/test/test.bw")
@@ -21,6 +20,17 @@ class BigWig(object):
     array('f', [0.10000000149011612, 0.20000000298023224, 0.30000001192092896, nan, nan, nan, nan, nan, nan])
     >>> b.values("1", 0, 9, False)
     array('f', [0.10000000149011612, 0.20000000298023224, 0.30000001192092896])
+
+    >>> b.stats("1", 0, 9)
+    0.2000000054637591
+
+    >>> b.stats("1", 0, 9, stat="std")
+    0.10000000521540645
+
+    >>> b.stats("1", 0, 4, stat="coverage")
+    0.75
+    >>> b.stats("1", 0, 4, stat="coverage", nBins=2)
+    array('d', [1.0, 0.5])
 
     >>> b.close()
     """
@@ -47,6 +57,18 @@ class BigWig(object):
         if intervals.l != 0:
             a.fromstring(ffi.buffer(intervals.value[0:intervals.l]))
         lib.bwDestroyOverlappingIntervals(intervals)
+        return a
+
+    def stats(self, chrom, start, end, stat="mean", nBins=1):
+        ops = ("mean", "std", "max", "min", "coverage")
+        assert stat in ops, stat
+        itype = ops.index(stat)
+        res = lib.bwStats(self.bw, chrom, start, end, nBins, itype)
+        a = array.array('d')
+        a.fromstring(ffi.buffer(res[0:nBins]))
+        ffi.gc(res, lib.free)
+        if nBins == 1:
+            return a[0]
         return a
 
 if __name__ == "__main__":
